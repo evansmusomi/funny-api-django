d2e62e3ec87558861c4cdb9452c419546e52add2
Evans Musomi <evans.musomi@gmail.com>
2018-07-21 07:32:31 +0300

    feat: adds jokecategory model and test

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import JokeCategory
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    """Defines the JokeCategory model test suite"""
+
+    def setUp(self):
+        """Defines the test client and variables"""
+        self.jokecategory_name = "Short Quotes"
+        self.jokecategory = JokeCategory(name=self.jokecategory_name)
+
+    def test_model_can_create_a_joke_category(self):
+        """Tests the JokeCategory model can create a Joke Category"""
+        initial_count = JokeCategory.objects.count()
+        self.jokecategory.save()
+        new_count = JokeCategory.objects.count()
+        self.assertNotEqual(initial_count, new_count)
